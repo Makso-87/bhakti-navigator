66ef91954b34b5feb5dd74f52282d81d1d2d0434
Maksim Popov <makso16108@gmail.com>
2022-05-24 13:07:08 +0300

    Deleted next-images using from next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withImages = require('next-images')
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = withImages({
-    trailingSlash: true,
-})
-
-
-module.exports = nextConfig
+module.exports = nextConfig;
