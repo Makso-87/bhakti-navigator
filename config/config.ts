38e21c11997616c3e9f1cd955395175eb2f66c09
Maksim Popov <makso16108@gmail.com>
2022-06-03 19:56:25 +0300

    Changed API_URL to test again

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 export default {
-  // API_URL: 'http://bhaktinavigator.tw1.ru/bhakti-navigator-wp',
-  API_URL: 'https://cp11466.tmweb.ru',
+  API_URL: 'https://bhaktinavigator.tw1.ru/bhakti-navigator-wp',
+  // API_URL: 'https://cp11466.tmweb.ru',
   WP_API_JSON: '/wp-json/wp/v2',
 };
