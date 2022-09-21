2167d6b4fc4dd0060c9f2f2dcdbc7a09f07c4ac4
Maksim Popov <makso16108@gmail.com>
2022-09-21 20:00:16 +0300

    Added bhakti way and points to it.

--- components/CoursesItemPage/MainScreen/MainScreen.tsx
+++ components/CoursesItemPage/MainScreen/MainScreen.tsx
@@ -26,6 +26,33 @@ export const MainScreen = (props) => {
 
   const bhaktiLevelsUnique = getUniqBhakyiLevels(bhakti_level);
 
+  const getBhaktiLevel = (name) => {
+    return bhaktiLevelsUnique.filter((item) => {
+      const ddd = item?.acf?.value.includes(name);
+      return item?.acf?.value === name || ddd;
+    });
+  };
+
+  const classesBeforeShraddha = cn(classes.Point, classes.BeforeShraddha, {
+    [classes.Active]: getBhaktiLevel('before_shraddha').length,
+  });
+
+  const classesShraddha = cn(classes.Point, classes.Shraddha, {
+    [classes.Active]: getBhaktiLevel('shraddha').length,
+  });
+
+  const classesSadhusanga = cn(classes.Point, classes.Sadhusanga, {
+    [classes.Active]: getBhaktiLevel('sadhu_sanga').length,
+  });
+
+  const classesBhadjanaKriya = cn(classes.Point, classes.BhadjanaKriya, {
+    [classes.Active]: getBhaktiLevel('bhadjana_kriya').length,
+  });
+
+  const classesAnarthaNivritti = cn(classes.Point, classes.AnarthaNivritti, {
+    [classes.Active]: getBhaktiLevel('anartha_nivritti').length,
+  });
+
   return (
     <div className={classes.MainScreen}>
       <div className={classes.BgBigCircle} />
@@ -58,23 +85,66 @@ export const MainScreen = (props) => {
 
           <div className={classes.RightSide}>
             <div className={classes.CourseInfoExtended}>
-              <div className={classes.Icons}>
-                {bhaktiLevelsUnique?.length
-                  ? bhaktiLevelsUnique.map((item) => {
-                      const classesIcon = cn(classes.Icon, {
-                        [classes.BeforeShraddha]: item?.acf?.value === 'before_shraddha',
-                        [classes.Shraddha]: item?.acf?.value === 'shraddha',
-                        [classes.Sadhusanga]: item?.acf?.value === 'sadhu_sanga',
-                        [classes.BhadjanaKriya]: item?.acf?.value.includes('bhadjana_kriya'),
-                        [classes.AnarthaNivritti]: item?.acf?.value === 'anartha_nivritti',
-                        [classes.Medium]: bhaktiLevelsUnique.length > 2,
-                        [classes.Small]: bhaktiLevelsUnique.length > 4,
-                      });
+              <div className={classes.Map}>
+                <div className={classesBeforeShraddha}>
+                  <div className={classes.Text}>Знакомство</div>
+                </div>
 
-                      return <div key={item.id} className={classesIcon} />;
-                    })
-                  : null}
+                <div className={classesShraddha}>
+                  <div className={classes.Text}>Шраддха</div>
+                </div>
+
+                <div className={classesSadhusanga}>
+                  <div className={classes.Text}>Садху-санга</div>
+                </div>
+
+                <div className={classesBhadjanaKriya}>
+                  <div className={classes.Text}>Бхаджана-крия</div>
+                </div>
+
+                <div className={classesAnarthaNivritti}>
+                  <div className={classes.Text}>Анартха-нивритти</div>
+                </div>
+
+                <div className={`${classes.Point} ${classes.Nistha}`}>
+                  <div className={classes.Text}>Ништха</div>
+                </div>
+
+                <div className={`${classes.Point} ${classes.Ruchi}`}>
+                  <div className={classes.Text}>Ручи</div>
+                </div>
+
+                <div className={`${classes.Point} ${classes.Asakti}`}>
+                  <div className={classes.Text}>Асакти</div>
+                </div>
+
+                <div className={`${classes.Point} ${classes.Bhava}`}>
+                  <div className={classes.Text}>Бхава</div>
+                </div>
+
+                <div className={`${classes.Point} ${classes.Prema}`}>
+                  <div className={classes.Text}>Према</div>
+                </div>
               </div>
+
+              {/*<div className={classes.Icons}>*/}
+              {/*  {bhaktiLevelsUnique?.length*/}
+              {/*    ? bhaktiLevelsUnique.map((item) => {*/}
+              {/*        const classesIcon = cn(classes.Icon, {*/}
+              {/*          [classes.BeforeShraddha]: item?.acf?.value === 'before_shraddha',*/}
+              {/*          [classes.Shraddha]: item?.acf?.value === 'shraddha',*/}
+              {/*          [classes.Sadhusanga]: item?.acf?.value === 'sadhu_sanga',*/}
+              {/*          [classes.BhadjanaKriya]: item?.acf?.value.includes('bhadjana_kriya'),*/}
+              {/*          [classes.AnarthaNivritti]: item?.acf?.value === 'anartha_nivritti',*/}
+              {/*          [classes.Medium]: bhaktiLevelsUnique.length > 2,*/}
+              {/*          [classes.Small]: bhaktiLevelsUnique.length > 4,*/}
+              {/*        });*/}
+
+              {/*        return <div key={item.id} className={classesIcon} />;*/}
+              {/*      })*/}
+              {/*    : null}*/}
+              {/*</div>*/}
+
               <div className={classes.Place}>{location}</div>
               <div className={classes.BriefDescription}>{format_briefing}</div>
             </div>
